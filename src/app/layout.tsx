1d0e88bd44a5d6a56bc23606774b2e68f4246ce6
Rajab Raza <zubairkhatri92+github@gmail.com>
2024-10-26 20:23:53 +0500

    Update1 layout.tsx

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -15,7 +15,28 @@ const geistMono = localFont({
 
 export const metadata: Metadata = {
   title: "AR Developers",
-  description: "AR Developers is a cutting-edge web design and software development agency",
+  description: "AR Test",
+  openGraph: {
+    title: "AR Developers",
+    description: "AR Test",
+    url: "https://ardevelopers.vercel.app", // Updated URL
+    siteName: "AR Developers",
+    images: [
+      {
+        url: "https://raw.githubusercontent.com/the-rajabraza/id_card_generator/main/socialcard.png", // Direct link to image
+        width: 1200,
+        height: 630,
+        alt: "AR Developers Social Card",
+      },
+    ],
+    type: "website",
+  },
+  twitter: {
+    card: "summary_large_image", // or "summary"
+    title: "AR Developers",
+    description: "AR Test",
+    image: "https://raw.githubusercontent.com/the-rajabraza/id_card_generator/main/socialcard.png", // Direct link to image
+  },
 };
 
 export default function RootLayout({
