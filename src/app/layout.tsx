3c39719a1c8d217d9ff3ae453710fc5777c9e2e9
Rajab Raza <zubairkhatri92+github@gmail.com>
2024-10-26 19:28:39 +0500

    Update layout.tsx

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -16,6 +16,26 @@ const geistMono = localFont({
 export const metadata: Metadata = {
   title: "AR Developers",
   description: "AR Developers Website",
+  openGraph: {
+    title: "AR Developers",
+    description: "AR Developers Website",
+    url: "https://ardevelopers.vercel.app", // Replace with your page URL
+    images: [
+      {
+        url: "https://github.com/the-rajabraza/my-ar-dev/blob/main/socialcard.png", // Replace with the URL of your image
+        width: 1200, // Optional
+        height: 630, // Optional
+        alt: "AR Developers Image", // Optional
+      },
+    ],
+    siteName: "AR Developers",
+  },
+  twitter: {
+    card: "summary_large_image",
+    title: "AR Developers",
+    description: "AR Developers Website",
+    images: ["https://github.com/the-rajabraza/my-ar-dev/blob/main/socialcard.png"], // Replace with the URL of your image
+  },
 };
 
 export default function RootLayout({
