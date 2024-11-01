eea76b23f0ba1c594eb953ddd58b9895c17e508a
Rajab Raza <zubairkhatri92+github@gmail.com>
2024-11-01 22:12:41 +0500

    Update Title And Description

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,5 +1,12 @@
-import ARDevelopersLanding from './components/ARDevelopersLanding';
+import type { Metadata } from 'next'
+import ARDevelopersLanding from './components/ARDevelopersLanding'
+
+export const metadata: Metadata = {
+  title: 'AR Developers',
+  description: 'AR Developers Landing Page',
+}
+
 
 export default function Home() {
-  return <ARDevelopersLanding />;
+  return <ARDevelopersLanding />
 }
